d713014bedbcd6d1e63bd451498026064114b9b6
Guilhem <guilhem.goulabert@gmail.com>
2026-06-12 21:16:38 +0200

    change script to create database

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "create schema in database"
+php bin/console doctrine:database:create
+
+echo "migrate entities"
+php bin/console make:migration
+php bin/console doctrine:migrations:migrate
+
 echo "start symfony"
 symfony server:stop
 symfony run -d --watch=config,src,templates,vendor symfony console messenger:consume async -vv
